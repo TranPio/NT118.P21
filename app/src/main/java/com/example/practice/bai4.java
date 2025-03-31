418405daca06869d2cbde069ed7aae278950bda4
Trần Hoài Phú <phubao789@gmail.com>
2025-03-31 09:53:17 +0700

    Commit update bai2, bai3, bai4

--- app/src/main/java/com/example/practice/bai4.java
+++ app/src/main/java/com/example/practice/bai4.java
@@ -8,12 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class bai2 extends AppCompatActivity {
+public class bai4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_bai2);
+        setContentView(R.layout.activity_bai4);
     }
 }
