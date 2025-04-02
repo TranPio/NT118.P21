692c9bcfa67453ec6676e95f2bab0ec125a1f76a
Trần Hoài Phú <phubao789@gmail.com>
2025-04-02 11:03:37 +0700

    Commit chinh sua giao dien bai4 tiep theo

--- app/src/main/java/com/example/practice/bai4.java
+++ app/src/main/java/com/example/practice/bai4.java
@@ -1,6 +1,10 @@
 package com.example.practice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +17,19 @@ public class bai4 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bai4);
+
+        GridLayout gridLayout = findViewById(R.id.gridKeys);
+        int childCount = gridLayout.getChildCount();
+
+        for (int i = 0; i < childCount; i++) {
+            View view = gridLayout.getChildAt(i);
+            if (view instanceof Button) {
+                Button btn = (Button) view;
+                String tag = (String) btn.getTag();
+                btn.setText(tag);  // Gán text = tag
+            }
+        }
     }
+
 }
