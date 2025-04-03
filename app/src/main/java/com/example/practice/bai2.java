ba9f42d2799d119c6451b181acb31746add5a570
Trần Hoài Phú <phubao789@gmail.com>
2025-04-03 10:24:09 +0700

    Commit update code bai 2

--- app/src/main/java/com/example/practice/bai2.java
+++ app/src/main/java/com/example/practice/bai2.java
@@ -4,16 +4,13 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class bai2_1 extends AppCompatActivity {
+public class bai2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_bai21);
+        setContentView(R.layout.activity_bai2);
     }
 }
